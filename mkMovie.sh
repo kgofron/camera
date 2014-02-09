433507ec676b913d485d08582a028f9ad156baa8
Kaz Gofron <kgofron@comcast.net>
2014-02-08 23:32:40 -0500

    avconv methods

--- mkMovie.sh
+++ mkMovie.sh
@@ -5,3 +5,6 @@ ffmpeg -r 1/5 -i img%03d.jpg -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
 
 #avconv works
 avconv -r 1 -i img%d.jpg -r 24 -s 704x480 -vsync cfr video.mpg
+avconv -r 30 -i hxn-cam1/pic_folder/img%d.jpg -r 30 -s 704x480 -
+vsync cfr video.mp4
+avconv -r 10 -i img%d.jpg -b:v 1000k test.mp4
