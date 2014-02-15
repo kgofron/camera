30d8a0abc48482c3595cf6514b5bfb770635cac0
Kaz Gofron <kgofron@comcast.net>
2014-02-15 18:15:50 -0500

    set linux system time

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Processing images using python, and ffmpeg (..)
 1. /etc/init.d/cameras
 2. /usr/local/bin/cameras.sh
 
+Make sure to set the date/time correctly
+sudo date -s"2014-2-15 18:12:25"
--- mkMovie.sh
+++ mkMovie.sh
@@ -8,3 +8,15 @@ avconv -r 1 -i img%d.jpg -r 24 -s 704x480 -vsync cfr video.mpg
 avconv -r 30 -i hxn-cam1/pic_folder/img%d.jpg -r 30 -s 704x480 -
 vsync cfr video.mp4
 avconv -r 10 -i img%d.jpg -b:v 1000k test.mp4
+
+# also works
+avconv -f image2 -i tmp/img%d.jpg -r 24 -s 704x480 v11.mp4
+
+# no dropped frames
+avconv -f image2 -r 1 -i hxn-cam1_v1/pic_folder/test%d.jpg -r 24 -s 704x480 v14.
+mp4
+
+# Using pipe (instead of ffplay, can use cat > out.mkv
+cat *.jpg | ffmpeg -f image2pipe -r 1/5 -c:v mjpeg -i - -c:v copy -f matroska - 
+| ffplay -
+
