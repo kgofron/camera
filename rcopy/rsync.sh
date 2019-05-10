bf4a54d0bda64148cd62d4ae94bd7e1304fb5edd
Kaz Gofron <kgofron@bnl.gov>
2019-05-10 18:42:22 -0400

    rsync script

--- rcopy/rsync.sh
+++ rcopy/rsync.sh
@@ -1,5 +1,5 @@
 # From synology system; Source has no "/", destination has "/"
-sudo rsync -a kgofron@xf10idd-ioc1.cs.nsls2.local:/IXS1 /volume1/IXS2/WinSys/
+sudo rsync -a kgofron@xf10idd-ioc1.cs.nsls2.local:/direct/IXS1 /volume1/IXS2/WinSys/
 
 # Used these to test
 sudo rsync -avn kgofron@xf10idd-ioc1.cs.nsls2.local:/IXS1/Data/2019-01-Cunsolo/ /volume1/IXS2/WinSys/IXS1/Data/2019-01-Cunsolo/
