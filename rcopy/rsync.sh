8c835618ddfba22398bbdc3af68dd0e72701a314
Kaz Gofron <kgofron@comcast.net>
2021-06-10 14:07:58 -0500

    rsync through proxy ssh

--- rcopy/rsync.sh
+++ rcopy/rsync.sh
@@ -5,3 +5,9 @@ sudo rsync -a kgofron@xf10idd-ioc1.cs.nsls2.local:/direct/IXS1 /volume1/IXS2/Win
 sudo rsync -avn kgofron@xf10idd-ioc1.cs.nsls2.local:/IXS1/Data/2019-01-Cunsolo/ /volume1/IXS2/WinSys/IXS1/Data/2019-01-Cunsolo/
 sudo rsync -av kgofron@xf10idd-ioc1.cs.nsls2.local:/IXS1/Data/2019-01-Cunsolo/ /volume1/IXS2/WinSys/IXS1/Data/2019-01-Cunsolo/
 sudo rsync -av kgofron@xf10idd-ioc1.cs.nsls2.local:/IXS1/Data/ /volume1/IXS2/WinSys/IXS1/Data/
+
+# rsync (private network)
+rsync -avR -e 'ssh -o "ProxyCommand 'ssh -W %h:%p ssh01"' d* [username]@daisy.warwick.ac.uk:/home/[username]
+
+# rsync (after Re-IP)
+rsync -avR d* [username]@daisy.warwick.ac.uk:/home/[username]
